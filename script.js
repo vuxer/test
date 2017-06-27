40b50660e60125bb3647ef290f4c734c3cdfd042
vuxer <avukota@yahoo.com>
2017-06-27 22:46:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var main=function() {
     if(navigator.userAgent.match(/Android/i)){
-        window.location.replace("www.youtube.com");
+        window.location.replace("Game for pc.html");
     }
     else {
         alert("pc");
