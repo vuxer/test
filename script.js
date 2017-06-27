5fb8dc3aeb5d6bb6f188f08f0d23e588c756a989
vuxer <avukota@yahoo.com>
2017-06-27 21:48:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var main=function() {
+    $('.icon-menu, img').click(function () {
+        $(".menu").animate({
+            left:"0px"},200);
+        $('body').animate({
+      left: '285px'
+    }, 200);
+    });
+    $("#zatvori").click(function()  {
+        $(".menu").animate({
+            left:'-285px'
+        },200);
+        $('body').animate({
+      left: '0px'
+    }, 200);
+    });
+};
+$(document).ready(main);
